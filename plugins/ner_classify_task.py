133550be6804301dd6eab4279a45d26acaa9cba7
chenhao <chenhao@aidigger.com>
2019-12-04 15:31:46 +0800

    add pony plugin

--- plugins/ner_classify_task.py
+++ plugins/ner_classify_task.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 -------------------------------------------------
-   File Name：     ner_task
+   File Name：     ner_classify_task
    Description :
    Author :       chenhao
-   date：          2019-11-28
+   date：          2019-12-04
 -------------------------------------------------
    Change Activity:
-                   2019-11-28:
+                   2019-12-04:
 -------------------------------------------------
 """
-
-from eigen_nltk.experiment import NerExperiment
+from eigen_nltk.experiment import NerClassifyExperiment
 from eigen_nltk.utils import read_config
 from plugin_sdk import ConfigParser
 
@@ -19,6 +18,7 @@ if __name__ == '__main__':
     parser = ConfigParser()
     job_configs = parser.parse()
     model_config_path = job_configs['args']['config_path']
+
     params = read_config(model_config_path)
-    experiment = NerExperiment(params)
+    experiment = NerClassifyExperiment(params)
     experiment.do_experiment()
