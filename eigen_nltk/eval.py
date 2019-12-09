21728b19770060e3f28ef4bb8b766c0ee1f914b8
chenhao <chenhao@aidigger.com>
2019-12-09 14:55:18 +0800

    sync

--- eigen_nltk/eval.py
+++ eigen_nltk/eval.py
@@ -97,8 +97,8 @@ def add_ner_pred(data, pred, contain_span=True):
 
         rs = dict(id=e['id'], content=e['content'], statistic=statistic,
                   entity_list=e['entity_list'], ner_pred=p, ner_fp=fp, ner_fn=fn, ner_tp=tp)
-        if "given_entity_list" in e.keys():
-            rs.update(given_entity_list=e['given_entity_list'])
+        if "prefix" in e.keys():
+            rs.update(given_entity_list=e['prefix'])
         rs_list.append(rs)
     return rs_list
 
